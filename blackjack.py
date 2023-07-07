5744741aca77af1e6cc8f66b63662de2b017ccec
Josh Thephasdin <47870092+tthephas@users.noreply.github.com>
2023-07-06 20:36:45 -0500

    logic for getting an ace added

--- blackjack.py
+++ blackjack.py
@@ -81,6 +81,24 @@ for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append((deal_card()))
 
+# Hint 6: Create a function called calculate_score() that takes a List of cards as input
+# and returns the score.
+# Look up the sum() function to help you do this.
+
+def calculate_score(cards):
+
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    # Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+
 
 
 
